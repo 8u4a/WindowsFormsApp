6c28c05602578d5ab4592ad3f05f58a6228c0d2f
8u4a <iordanalecsandru@yahoo.com>
2019-03-10 16:47:32 +0200

    windows form app

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblBrands = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCarBrand = new System.Windows.Forms.TextBox();
+            this.btnCreateCar = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.numYear = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numYear)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "New Car";
+            // 
+            // lblBrands
+            // 
+            this.lblBrands.AutoSize = true;
+            this.lblBrands.Location = new System.Drawing.Point(3, 42);
+            this.lblBrands.Name = "lblBrands";
+            this.lblBrands.Size = new System.Drawing.Size(35, 13);
+            this.lblBrands.TabIndex = 1;
+            this.lblBrands.Text = "Brand";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Year";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtCarBrand
+            // 
+            this.txtCarBrand.Location = new System.Drawing.Point(6, 59);
+            this.txtCarBrand.Name = "txtCarBrand";
+            this.txtCarBrand.Size = new System.Drawing.Size(100, 20);
+            this.txtCarBrand.TabIndex = 3;
+            this.txtCarBrand.TextChanged += new System.EventHandler(this.txtCarBrand_TextChanged);
+            // 
+            // btnCreateCar
+            // 
+            this.btnCreateCar.Location = new System.Drawing.Point(6, 135);
+            this.btnCreateCar.Name = "btnCreateCar";
+            this.btnCreateCar.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateCar.TabIndex = 5;
+            this.btnCreateCar.Text = "Create Car";
+            this.btnCreateCar.UseVisualStyleBackColor = true;
+            this.btnCreateCar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.ForeColor = System.Drawing.Color.Red;
+            this.lblResult.Location = new System.Drawing.Point(6, 184);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 6;
+            // 
+            // numYear
+            // 
+            this.numYear.Location = new System.Drawing.Point(6, 98);
+            this.numYear.Maximum = new decimal(new int[] {
+            1900,
+            0,
+            0,
+            0});
+            this.numYear.Minimum = new decimal(new int[] {
+            1900,
+            0,
+            0,
+            0});
+            this.numYear.Name = "numYear";
+            this.numYear.Size = new System.Drawing.Size(120, 20);
+            this.numYear.TabIndex = 7;
+            this.numYear.Value = new decimal(new int[] {
+            1900,
+            0,
+            0,
+            0});
+            this.numYear.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 431);
+            this.Controls.Add(this.numYear);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCreateCar);
+            this.Controls.Add(this.txtCarBrand);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblBrands);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numYear)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblBrands;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCarBrand;
+        private System.Windows.Forms.Button btnCreateCar;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.NumericUpDown numYear;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+            
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtCarBrand.Text))
+            {
+                lblResult.Text = "Please insert car brand";
+                return;
+            }
+
+            var bmw = new Car(txtCarBrand.Text.ToUpperInvariant(), Convert.ToInt32(numYear.Value));
+
+            var serializedCars = JsonConvert.SerializeObject(bmw);
+            lblResult.Text = serializedCars;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numYear.Maximum = DateTime.Now.Year;
+            var manager = new AWSManager();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCarBrand_TextChanged(object sender, EventArgs e)
+        {
+            lblResult.Text = string.Empty;
+        }
+    }
+}
